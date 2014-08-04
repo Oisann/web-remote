98841c6303c2f3d76f186e3d257de4dc2bfe0c92
Jonas <jonas@oisann.net>
2014-08-04 03:21:20 +0200

    Revert "Prevent landscape mode"
    This reverts commit 91809afc52ddade21ae7d6749e8afb3e9ba2565b.

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,16 +1,3 @@
 var host = "oisann.github.io";
 if ((host == window.location.host) && (window.location.protocol != "https:"))
     window.location.protocol = "https";
-
-$(window).bind('orientationchange', function(e, onready){
-   if(onready){
-       $(document.body).addClass('portrait-onready');
-   }
-   if (Math.abs(window.orientation) != 90){
-       $(document.body).addClass('portrait');
-   } 
-   else {
-       $(document.body).removeClass('portrait').removeClass('portrait-onready');
-   }
-});
-$(window).trigger('orientationchange', true); // fire the orientation change event at the start, to make sure 
