db50bac05cf962239517aa58a73577ea1f2c79e2
Jonas <jonas@oisann.net>
2014-08-04 04:24:04 +0200

    Webapp ready

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,3 +1,19 @@
-var host = "oisann.github.io";
+﻿var host = "oisann.github.io";
 if ((host == window.location.host) && (window.location.protocol != "https:"))
     window.location.protocol = "https";
+
+var snapper = new Snap({
+	element: $('.panel'),
+	disable: 'right'
+});
+
+$('.menu-button').click(function(){
+	snapper.open('left');
+});
+
+setTimeout(function(){
+	$('.splash').fadeOut();
+	setTimeout(function(){
+		$('.splash').remove();
+	}, 1000);
+}, 5000);
