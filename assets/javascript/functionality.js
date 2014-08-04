07c1db8822ab2097d47f1df9d9ccfd6cb7b692b6
Jonas <jonas@oisann.net>
2014-08-04 04:57:21 +0200

    Local storage and GUID

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -1,11 +1,29 @@
 ﻿var host = "oisann.github.io";
-if ((host == window.location.host) && (window.location.protocol != "https:"))
-    window.location.protocol = "https";
+if ((host == window.location.host) && (window.location.protocol != "https:")) window.location.protocol = "https";
 
 var snapper = new Snap({
 	element: $('.panel'),
 	disable: 'right'
 });
+
+function createGuid(){
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+		return v.toString(16);
+	});
+}
+
+var guid = localStorage.getItem('guid');
+if(!guid){
+	localStorage.setItem('guid', createGuid());
+	guid = localStorage.getItem('guid');
+}
+
+$('#name').val(localStorage.getItem('devicename'));
+if($('#name').val() !== ''){
+	$('#name').attr('disabled', '');
+	$('#passcode').focus();
+}
 
 $('.menu-button').click(function(){
 	snapper.open('left');
@@ -22,13 +40,14 @@ $('#login').submit(function(e){
 			$('#name').val('');
 		}, 1000);
 	} else {
+		localStorage.setItem('devicename', name);
 		//Send socket with name and passcode
 	}
 });
 
 setTimeout(function(){
-	$('.splash').fadeOut();
+	$('.splash').fadeOut(1000);
 	setTimeout(function(){
 		$('.splash').remove();
 	}, 1000);
-}, 5000);
+}, 4000);
