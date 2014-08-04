2257b2bb1bee7a7c709c34f56e3a0849a9493ca8
Jonas <jonas@oisann.net>
2014-08-04 04:38:56 +0200

    Login submit
    + new favicon

--- assets/javascript/functionality.js
+++ assets/javascript/functionality.js
@@ -11,6 +11,21 @@ $('.menu-button').click(function(){
 	snapper.open('left');
 });
 
+$('#login').submit(function(e){
+	e.preventDefault();
+	var name = $('#name').val(),
+		passcode = $('#passcode').val();
+	if(name.length <= 1 || passcode.length <= 1){
+		$('#name').val('Invalid name/passcode');
+		$('#passcode').val('');
+		setTimeout(function(){
+			$('#name').val('');
+		}, 1000);
+	} else {
+		//Send socket with name and passcode
+	}
+});
+
 setTimeout(function(){
 	$('.splash').fadeOut();
 	setTimeout(function(){
